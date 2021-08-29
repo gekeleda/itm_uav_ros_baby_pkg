fixed trajectory frequency, added position error calculation

scripts/acados/quadrotor_optimizer.py
@@ -227,7 +227,7 @@ class QuadOptimizer:
 
     def mpc_estimation_loop(self, mpc_iter):
         if self.trajectory_path is not None and self.current_state is not None:
-            time_1 = time.time()
+            # time_1 = time.time()
             # dt = 0.1
             current_trajectory = self.trajectory_path
 
@@ -239,7 +239,7 @@ class QuadOptimizer:
             new_state[8] = self.pendulum_state[2]-self.current_state[3]
             new_state[9] = self.pendulum_state[3]-self.current_state[4]
 
-            simX[mpc_iter+1] = new_state
+            simX[mpc_iter] = new_state # mpc_iter+1 ?
             simD[mpc_iter] = current_trajectory[0]
 
             # print()
@@ -251,7 +251,9 @@ class QuadOptimizer:
             # print("current trajectory: ")
             # print(current_trajectory[-1])
 
-
+            tra = current_trajectory[0]
+            # error_xyz = np.linalg.norm(np.array([new_state[0] - tra[0], new_state[1] - tra[1], new_state[2] - tra[2]]))
+            # print(error_xyz)
 
             self.solver.set(self.N, 'yref', current_trajectory[-1, :6])
             for i in range(self.N):
@@ -275,7 +277,7 @@ class QuadOptimizer:
 
                 print()
                 print("current trajectory: ")
-                print(current_trajectory[-1])
+                print(current_trajectory[0])
 
 
                 self.att_command.orientation = Quaternion(

scripts/acados/quadrotor_trajectory.py
@@ -5,20 +5,20 @@ from itm_mav_msgs.msg import itm_trajectory_msg
 from itm_mav_msgs.msg import itm_trajectory_point
 from std_msgs.msg import Header
 
-delay_init = 150
-delay_traj = 275
+delay_init = 1500
+delay_traj = 2750
 n_nodes = 20
 
 reference_point = np.array([1.0, 0.0, 1.0, 0., 0., 0., 0., 0., 0., 0., 0., 0., 0.])
 
-vel = 0.4
+vel = 0.04
 veliter = vel/n_nodes
 dist = np.linalg.norm(reference_point[:6])
 steps = int(dist/veliter)
 
 def trajectory_generator(iter_n, current_trajectory, traj_shape='circle'):
     if traj_shape=='spiral':
-        traj_fac = 30 # 30
+        traj_fac = 300 # 30
         kz = 0.005
         next_trajectories = current_trajectory[1:, :]
         # next_trajectories = np.concatenate((next_trajectories,
@@ -27,7 +27,7 @@ def trajectory_generator(iter_n, current_trajectory, traj_shape='circle'):
         np.array([np.cos((iter_n)/traj_fac), np.sin((iter_n)/traj_fac), 1.0+kz*iter_n, -np.sin(iter_n/traj_fac), np.cos(iter_n/traj_fac), 0.0, 0., 0., 0.0, 0.0, 0.0, 0.0, 0.0]).reshape(1, -1)))
         return next_trajectories
     elif traj_shape=='eight':
-        traj_fac = 30 # 30
+        traj_fac = 300 # 30
         next_trajectories = current_trajectory[1:, :]
         # next_trajectories = np.concatenate((next_trajectories,
         # np.array([np.cos((iter_n)/traj_fac), np.sin((iter_n)/traj_fac), 1.0, 0.0, 0.0, 0.0, 0., 0., 0.0, 0.0, 0.0, 0.0, 0.0]).reshape(1, -1)))
@@ -35,7 +35,7 @@ def trajectory_generator(iter_n, current_trajectory, traj_shape='circle'):
         np.array([np.cos((iter_n)/traj_fac), 0.5*np.sin(2*(iter_n)/traj_fac), 1.0, -np.sin(iter_n/traj_fac), np.cos(2*iter_n/traj_fac), 0.0, 0., 0., 0.0, 0.0, 0.0, 0.0, 0.0]).reshape(1, -1)))
         return next_trajectories
 
-    traj_fac = 30 # 30
+    traj_fac = 300 # 30
     next_trajectories = current_trajectory[1:, :]
     # next_trajectories = np.concatenate((next_trajectories,
     # np.array([np.cos((iter_n)/traj_fac), np.sin((iter_n)/traj_fac), 1.0, 0.0, 0.0, 0.0, 0., 0., 0.0, 0.0, 0.0, 0.0, 0.0]).reshape(1, -1)))
@@ -87,7 +87,7 @@ def talker():
     pub = rospy.Publisher('/robot_trajectory',
                           itm_trajectory_msg, queue_size=1)
     rospy.init_node('trajectory_node')
-    dt = 0.1
+    dt = 0.01
     rate = rospy.Rate(1/dt)
 
     start_point = np.zeros_like(reference_point)